e7b59ce79772e0a41fc9016e6fbeb67865522777
Michael Sherwood <mjsherwood@gmail.com>
2022-05-05 07:54:01 -0700

    Update card.py
    I added comments and fixed the pull_card logic. Also added method for user input on Hi or Low

--- cards/game/card.py
+++ cards/game/card.py
@@ -1,14 +1,21 @@
 import random
 
+"""class Card is used to create the Card object with values and gets
+user guess of Hi or Lo and finally declares points based on what was retrieved."""
 class Card():
 
+    """Creates card object and declares attributes."""
     def __init__(self):
         self.value = 0
         self.points = 0
-        self.guess = 
+        self.guess = 0
 
-    def guess(self)
+    """method to to collect input for which """
+    def guess(self):
+        self.guess = input("Hi or Low: ")
 
+    """method to pull a card with a random number between 1 and 13.
+    It then will assign points based on the guess that was made with the guess method."""
     def pull_card(self):
         self.value = random.randint(1, 13)
-        self.points = 50 if self.value == 6 else 100 if self.value == 1 else 0
+        self.points = 100 if self.guess == "Hi" and self.guess > self.value else 100 if self.guess == "Low" and self.guess < self.value else -75

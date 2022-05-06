c986542b978737ad5268680f30c0abe9ce4a0ab2
TonaGonzalez <tonaglezc@gmail.com>
2022-05-06 17:50:59 -0600

    -Tonatiuh Gonzalez

--- cards/game/card.py
+++ cards/game/card.py
@@ -6,18 +6,18 @@ class Card():
 
     """Creates card object and declares attributes."""
     def __init__(self):
-        self.newvalue = 0
-        self.oldvalue = 0
+        self.new_value = 0
+        self.old_value = random.randint(1,13)
         self.points = 0
+        self.guess = ""
 
     """method to to collect input for which """
-    def guess(self):
-        self.guess = input("Hi or Low: ")
+    def get_guess(self):
+        self.guess = input("Higher or Lower: [h/l] ")
 
     """method to pull a card with a random number between 1 and 13.
     It then will assign points based on the guess that was made with the guess method."""
     def pull_card(self):
-        self.newvalue = random.randint(1, 13)
-        self.points = 100 if self.guess == "Hi" and self.newvalue > self.oldvalue else 100 if self.guess == "Low" and self.newvalue < self.oldvalue else -75
-
+        self.new_value = random.randint(1, 13)
+        self.points = 100 if self.guess == "h" and self.new_value > self.old_value else 100 if self.guess == "l" and self.new_value < self.old_value else -75
 
--- cards/game/card_director.py
+++ cards/game/card_director.py
@@ -7,7 +7,8 @@ class CardDirector():
         #What other things need to be updated?
         self.is_playing = True
         self.score = 300
-        self.total_score = ""
+        self.total_score = 300
+        self.card = Card()                              #Created the object card from class Card
 
     def start_game(self):
         while self.is_playing:
@@ -16,22 +17,26 @@ class CardDirector():
             self.do_outputs()
 
     def get_inputs(self):
-        draw_card = input("Draw card? [y/n] ")
-        self.is_playing = (draw_card == "y")
+        print(f'The card is: {self.card.old_value}')
+        self.card.get_guess()
+
+        # Change this to "Play again: [y/n]"
+        # draw_card = input("Draw card? [y/n] ")
+        # self.is_playing = (draw_card == "y")
 
     def do_updates(self):
         if not self.is_playing:
             return
+        self.card.pull_card()
         #We need to do the part for doing the updates.
 
     def do_outputs(self):
         if not self.is_playing:
             return
 
-        values = ""
         #We need to figure out how to get values and what values.
-
-        print(f'You drew: {values}')
-        print(f'Your score is: {self.score}\n')
+        print(f"Next card was: {self.card.new_value}")
+        print(f'Your score is: {self.card.points}')
         print(f'Your total score is: {self.total_score}')
-        self.is_playing == (self.total_score > 0)
+        print()
+        self.is_playing = (self.total_score > 0) 
